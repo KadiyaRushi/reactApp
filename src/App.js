9111ce6d56eb93dae1a4fce84d2f7c2d091831d2
KadiyaRushi <rushi.cilans@gmail.com>
2023-01-23 14:42:06 +0530

    Add List

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
+  const productList = [
+    { name: "Spaker", prise: 20000 },
+    { name: "Speaker cover", prise: 2000 },
+    { name: "IPhone", prise: 130000 },
+    { name: "Table Fan", prise: 3000 },
+  ];
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Product List</h1>
+      <div className="nameAndPrise">
+        <h3>Name</h3>
+        <h3>Prise</h3>
+      </div>
+
+      {productList.map((item) => {
+        console.log(item.name);
+        return (
+          <div className="flex_container">
+            <h4>{item.name}</h4>
+
+            <p>₹ {item.prise}</p>
+          </div>
+        );
+      })}
     </div>
   );
 }
